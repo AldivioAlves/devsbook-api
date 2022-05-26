9dcfb9459395406be5f8520b19972994112eeccc
Aldivio <aldivio89@hotmail.com>
2022-05-26 14:54:21 -0300

    upload user avatar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Image;
+
 class UserController extends Controller
 {
     private $loggedUser;
@@ -70,5 +72,37 @@ class UserController extends Controller
         $user->save();
         $result['user']=$user;
         return $this->sendResponse($result,200);
+    }
+
+    public function updateAvatar(Request $request){
+        $result = ['error'=>''];
+
+        $allowTypes = ['image/jpeg','image/jpg','image/png'];
+
+        $image = $request->file('avatar');
+
+        if(!$image){
+            $result['error'] = 'Imagem não enviada.';
+            return $this->sendResponse($result,400);
+        }
+
+        if(!in_array($image->getClientMimeType(),$allowTypes)){
+            $result['error'] = 'Arquivo não suportado.';
+            return $this->sendResponse($result,400);
+        }
+        $filename = md5(time().rand(0,9999)).'jpg';
+        $destPath = public_path('/media/avatars');
+
+        $img = Image::make($image->path())
+        ->fit(200,200)
+        ->save($destPath.'/'.$filename);
+
+        $user = User::find($this->loggedUser['id']);
+        $user->avatar = $filename;
+        $user->save();
+
+        $result['url'] = url('/media/avatars/'.$filename);
+
+        return $result;
     }
 }
